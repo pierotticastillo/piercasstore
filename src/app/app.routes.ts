cecfd25dbc510611d24ef748a4da71b5f3dc7b6f
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2025-02-18 20:34:32 -0300

    Update: - Sumar, restar cantidades del carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,10 @@ export const routes: Routes = [
       import('./products/features/product-shell/product.route'),
   },
   {
+    path: 'cart',
+    loadComponent: () => import('./cart/cart.component'),
+  },
+  {
     path: '**',
     redirectTo: '',
   },
--- src/app/shared/data-access/cart-state.service.ts
+++ src/app/shared/data-access/cart-state.service.ts
@@ -20,6 +20,10 @@ export class CartStateService {
       products,
       loaded: true,
     })),
+    catchError((error) => {
+      console.error('Error loading products:', error);
+      return of(this.initialState);
+    }),
   );
 
   state = signalSlice({
@@ -28,27 +32,55 @@ export class CartStateService {
     actionSources: {
       add: (state, action$: Observable<ProductItemCart>) =>
         action$.pipe(map((product) => this.add(state, product))),
+
+      remove: (state, action$: Observable<number>) =>
+        action$.pipe(map((id) => this.remove(state, id))),
+
+      update: (state, action$: Observable<ProductItemCart>) =>
+        action$.pipe(map((product) => this.update(state, product))),
     },
     effects: (state) => ({
       load: () => {
-				if (state().loaded) {
-					this._storageService.saveProducts(state().products)
-				}
-        console.log(state.products());
+        if (state().loaded) {
+          this._storageService.saveProducts(state().products);
+        }
+        console.log(state().products); // Corregido
       },
     }),
   });
 
   private add(state: Signal<StateCart>, product: ProductItemCart) {
-    const isInCart = state().products.find(
+    const existingProduct = state().products.find(
       (productInCart) => productInCart.product.id === product.product.id,
     );
-    if (!isInCart) {
-      return 	{ products:[...state().products, { ...product, quantity: 1 }]};
+    if (!existingProduct) {
+      return {
+        products: [...state().products, { ...product, quantity: 1 }],
+      };
     }
-    isInCart.quantity += 1;
     return {
-      products: [...state().products],
+      products: state().products.map((p) =>
+        p.product.id === product.product.id
+          ? { ...p, quantity: p.quantity + 1 }
+          : p,
+      ),
+    };
+  }
+
+  private remove(state: Signal<StateCart>, id: number) {
+    return {
+      products: state().products.filter((product) => product.product.id !== id),
+    };
+  }
+  private update(state: Signal<StateCart>, product: ProductItemCart) {
+    const products = state().products.map((productInCart) => {
+      if (productInCart.product.id === product.product.id) {
+        return { ...productInCart, quantity: product.quantity };
+      }
+      return productInCart;
+    });
+    return {
+      products,
     };
   }
 }
